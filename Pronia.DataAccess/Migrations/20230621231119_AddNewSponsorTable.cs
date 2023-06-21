493d53e7c9893eb30cb0d6d8e64f8533992316fd
Kerimov056 <ulvisk@code.edu.az>
2023-06-21 16:20:41 -0700

    Sponsor the end and Blog Start

---
+++ Pronia.DataAccess/Migrations/20230621231119_AddNewSponsorTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pronia.DataAccess.Migrations
+{
+    public partial class AddNewSponsorTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "sponsors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sponsors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sponsors");
+        }
+    }
+}
