8579c0512c796aadaf97947b0f6b7d0020379b9d
Kerimov056 <ulvisk@code.edu.az>
2023-06-22 20:42:47 -0700

    Admin Panel Auto Mapper

---
+++ Pronia/Areas/Admin/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Pronia.Areas.Admin.ProductViewModel;
+using Pronia.Core.Entities;
+
+namespace Pronia.Areas.Admin.AutoMapper;
+
+public class MappingProfile:Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<Product, ProductView>();
+		CreateMap<ProductView, Product>();
+	}
+}
